586becb6f944b4e8cc16faba005ea91e60ed63e3
Chidozie <chidough14@gmail.com>
2022-01-21 19:23:53 +0100

    display records and add pagination

--- src/App.js
+++ src/App.js
@@ -1,23 +1,92 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import { List, Button } from "antd"
+import 'antd/dist/antd.css';
+
+
 
 function App() {
+  const [records, setRecords] = useState([])
+  const [next, setNext] = useState(null)
+  const [previous, setPrevious] = useState(null)
+
+  useEffect(() => {
+    const fetchRecords = async () => {
+      await fetch('https://swapi.dev/api/people/', {
+        method: "GET",
+        headers: {"Content-Type": "application/json"}
+        })
+        .then(response => response.json()) 
+        .then(json => {
+          setRecords(json.results)
+          setNext(json.next)
+          setPrevious(json.previous)
+          console.log(json)
+        })
+        .catch(err => console.log(err));
+    }
+
+    fetchRecords()
+  }, [])
+
+
+  const goToNext = async (url) => {
+    await fetch(url, {
+        method: "GET",
+        headers: {"Content-Type": "application/json"}
+        })
+        .then(response => response.json()) 
+        .then(json => {
+          setRecords(json.results)
+          setNext(json.next)
+          setPrevious(json.previous)
+          console.log(json)
+        })
+        .catch(err => console.log(err));
+  }
+
+  const goToPrevious = async (url) => {
+    await fetch(url, {
+        method: "GET",
+        headers: {"Content-Type": "application/json"}
+        })
+        .then(response => response.json()) 
+        .then(json => {
+          setRecords(json.results)
+          setNext(json.next)
+          setPrevious(json.previous)
+          console.log(json)
+        })
+        .catch(err => console.log(err));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <List
+        itemLayout="horizontal"
+        dataSource={records}
+        renderItem={(item) => (
+          <List.Item
+          >
+            <List.Item.Meta
+              title={
+                <div>
+                  <p>Name : {item.name} </p>
+                  <p>Height : {item.height} </p>
+                  <p> Mass: {item.mass}</p>
+                </div>
+              }
+              description=""
+            />
+          </List.Item>
+        )}
+      />
+
+      <div style={{ display: "flex", justifyContent: "space-evenly"}}>
+        <Button type='danger' onClick={() => goToPrevious(previous)} disabled={previous === null ? true : false}>Previous</Button>
+        <Button type="primary" onClick={() => goToNext(next)} disabled={next === null ? true : false}>Next</Button>
+        
+      </div>
     </div>
   );
 }
