1068a34cb7155403f150208fe5317cb7bb5d22eb
Chidozie <chidough14@gmail.com>
2022-01-22 08:06:55 +0100

    add search functionality

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import './App.css';
 import { useEffect, useState } from 'react';
-import { List, Button, Modal } from "antd"
+import { List, Button, Modal, PageHeader, Input } from "antd"
 import 'antd/dist/antd.css';
-
+const { Search } = Input;
 
 
 function App() {
+
   const [records, setRecords] = useState([])
   const [next, setNext] = useState(null)
   const [previous, setPrevious] = useState(null)
   const [visible, setVisible] = useState(false)
   const [details, setDetails] = useState()
+  const [page, setPage] = useState(1)
+  const [loading, setLoading] = useState(false)
+ 
 
   useEffect(() => {
     const fetchRecords = async () => {
@@ -41,6 +45,9 @@ function App() {
           setRecords(json.results)
           setNext(json.next)
           setPrevious(json.previous)
+
+          let newPage = page + 1
+          setPage(newPage)
         })
         .catch(err => console.log(err));
   }
@@ -55,6 +62,9 @@ function App() {
           setRecords(json.results)
           setNext(json.next)
           setPrevious(json.previous)
+          
+          let newPage = page - 1
+          setPage(newPage)
         })
         .catch(err => console.log(err));
   }
@@ -73,8 +83,43 @@ function App() {
       .catch(err => console.log(err));
   }
 
+
+  const searchApi = async (value) => {
+    setLoading(true)
+
+    await fetch(`https://swapi.dev/api/people/?search=${value}`, {
+      method: "GET",
+      headers: {"Content-Type": "application/json"}
+      })
+      .then(response => response.json()) 
+      .then(json => {
+        setRecords(json.results)
+        setNext(json.next)
+        setPrevious(json.previous)
+        setPage(1)
+
+        setLoading(false)
+      })
+      .catch(err => console.log(err));
+  }
+
   return (
-    <div className="">
+    <div className="app">
+      <PageHeader
+        className="site-page-header"
+        title="Starwars Characters"
+      />
+
+      <div className="searchBox" >
+        <Search 
+          placeholder="Enter search item" 
+          size="large" 
+          enterButton
+          loading={loading} 
+          onSearch={searchApi}
+        />
+      </div>
+
       <List
         itemLayout="horizontal"
         dataSource={records}
@@ -95,6 +140,9 @@ function App() {
 
       <div style={{ display: "flex", justifyContent: "space-evenly"}}>
         <Button type='danger' onClick={() => goToPrevious(previous)} disabled={previous === null ? true : false}>Previous</Button>
+
+        <p>Page {page}</p>
+
         <Button type="primary" onClick={() => goToNext(next)} disabled={next === null ? true : false}>Next</Button>
         
       </div>
